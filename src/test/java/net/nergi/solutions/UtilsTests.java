6b17df2a80336e4cf833cde7795b07be1edc2662
Shots243 <fawwazandsyafiq@gmail.com>
2021-01-14 17:55:56 +0000

    removed redundand imports

--- src/test/java/net/nergi/solutions/UtilsTests.java
+++ src/test/java/net/nergi/solutions/UtilsTests.java
@@ -5,10 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import net.nergi.Utils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 public class UtilsTests {
 
