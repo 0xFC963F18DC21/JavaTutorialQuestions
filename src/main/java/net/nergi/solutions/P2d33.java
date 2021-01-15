12fd4b77f92e6c1bd0ad35dcf4e422caaacfd08b
Shots243 <fawwazandsyafiq@gmail.com>
2021-01-15 18:55:11 +0000

    replaced with deque

--- src/main/java/net/nergi/solutions/P2d33.java
+++ src/main/java/net/nergi/solutions/P2d33.java
@@ -3,7 +3,7 @@ package net.nergi.solutions;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Stack;
+import java.util.ArrayDeque;
 import net.nergi.Solution;
 
 @SuppressWarnings("unused")
@@ -22,7 +22,7 @@ public class P2d33 implements Solution {
    */
   @Override
   public void exec() {
-    Stack<String> lines = new Stack<>();
+    ArrayDeque<String> lines = new ArrayDeque<>();
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String line;
 
@@ -32,7 +32,7 @@ public class P2d33 implements Solution {
         line = br.readLine();
 
         if (line != null) {
-          lines.add(line);
+          lines.push(line);
         } else {
           br.close();
         }
