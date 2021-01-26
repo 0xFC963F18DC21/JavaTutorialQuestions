ce285d0a472c04d7aeb395088daec762522085b1
Shots243 <fawwazandsyafiq@gmail.com>
2021-01-26 13:43:56 +0000

    I SAID... DOCS!

--- src/main/java/net/nergi/misc/DequeRandomDemo.java
+++ src/main/java/net/nergi/misc/DequeRandomDemo.java
@@ -2,7 +2,7 @@ package net.nergi.misc;
 
 import net.nergi.misc.objects.DequeRandom;
 
-/** Demo class for {@link DequeRandom}. */
+/** Demo class for {@link DequeRandom}. Prints 24 numbers. */
 @SuppressWarnings("unused")
 public class DequeRandomDemo implements Runnable {
 
@@ -12,8 +12,8 @@ public class DequeRandomDemo implements Runnable {
     final DequeRandom drnd = new DequeRandom(4);
 
     // Output some numbers
-    System.out.println("16 random numbers from 0 to 6:");
-    for (int i = 0; i < 16; ++i) {
+    System.out.println("24 random numbers from 0 to 6:");
+    for (int i = 0; i < 24; ++i) {
       System.out.print(drnd.nextInt(7) + " ");
     }
 
